1e13b33b0e8e0adad4f1a0103d9455e6999241b6
captain13128 <61901168+captain13128@users.noreply.github.com>
2021-12-07 18:53:58 +0300

    Update median.sol

--- src/median.sol
+++ src/median.sol
@@ -270,3 +270,25 @@ contract MedianAAVEUSD is Median {
         );
     }
 }
+
+contract MedianMATICRUB is Median {
+    bytes32 public constant wat = "MATICRUB";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
+
+contract MedianWBTCRUB is Median {
+    bytes32 public constant wat = "WBTCRUB";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
